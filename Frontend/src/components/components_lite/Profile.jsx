416d3b1ffe32bc6dd68f9df3dd6e7fa9e80e19b8
aryastha77 <aryashrestha888@gmail.com>
2025-03-07 21:39:19 +0545

    Profile cards

--- Frontend/src/components/components_lite/Profile.jsx
+++ Frontend/src/components/components_lite/Profile.jsx
@@ -1,36 +1,49 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import { Button } from "../ui/button";
-import { Pen } from "lucide-react";
+import { Pen, Mail, Contact } from "lucide-react";
 
 const Profile = () => {
   return (
     <div>
       <Navbar />
-      <div className="mx-w-4xl max-auto bg-white border border-gray-300 rounded-2xl my-5 p-8 shadow shadow-grey-4 hover:shadow-[#E67E22]">
-        <div classsName="flex justify-between">
-          <div className="flex gap-4 items-center">
-            <Avatar className="h-12 w-12">
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
+
+      <div className="max-w-7xl mx-auto bg-white border-gray-200 rounded-2xl my-5 p-8 shadow shadow-gray-500 hover:shadow-[#d9731d]">
+        <div className="flex justify-between">
+          <div className=" flex items-center gap-5">
+            <Avatar className="cursor-pointer h-24 w-24 flex items-center justify-center rounded-full overflow-hidden border border-gray-300">
+              <AvatarImage
+                src="https://github.com/shadcn.png"
+                alt="profilepicture"
+              />
             </Avatar>
+
+            <div>
+              <h1 className="font-medium text-xl"> Full Name</h1>
+              <p className="text-gray-600">
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit veniam aperiam quaerat quibusdam et ducimus?
+              </p>
+            </div>
+
+            <Button variant="outline" className="text-right items-center">
+              <Pen />
+            </Button>
           </div>
-         
-            <h1 className="font-medium text-[#2C3E50]">Arya Shrestha</h1>
-            <div className="my-4 flex justify-between">
-            <p className="text-sm text-gray-500">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            </p>
+        </div>
+
+        <div children='my-5'>
+          <div className="flex items-center gap-3 my-2">
+            <Mail />
+            <span> aryashrestha888@gmail.com</span>
           </div>
 
-          <Button
-            onClick={() => Setopen(true)}
-            variant="outline"
-            className="text-right"
-          >
-            <Pen />
-          </Button>
+          <div className="flex items-center gap-3 my-2">
+            <Contact />
+            <span> 9864831310</span>
+          </div>
         </div>
       </div>
     </div>
