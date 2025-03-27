56451e1ffe864ca7072359f77925adb01c2958a6
aryastha77 <aryashrestha888@gmail.com>
2025-03-27 13:32:58 +0545

    User role - Recruiter and Student separated

--- Frontend/src/components/components_lite/Navbar.jsx
+++ Frontend/src/components/components_lite/Navbar.jsx
@@ -1,46 +1,42 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { Popover, PopoverTrigger, PopoverContent } from '../ui/popover';
-import { LogOut, User2 } from 'lucide-react';
-import {useDispatch, useSelector} from 'react-redux';
-import { toast } from 'sonner';
-import axios from 'axios';
-import { USER_API_ENDPOINT } from '@/utils/data';
-import { setUser } from '@/redux/authSlice';
-
+import { Popover, PopoverTrigger, PopoverContent } from "../ui/popover";
+import { LogOut, User2 } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
+import axios from "axios";
+import { USER_API_ENDPOINT } from "@/utils/data";
+import { setUser } from "@/redux/authSlice";
 
 const Navbar = () => {
-  
-  const {user} =useSelector((store) => store.auth);
+  const { user } = useSelector((store) => store.auth);
 
   const navigate = useNavigate();
-  const dispatch =useDispatch();
+  const dispatch = useDispatch();
 
-  const logoutHandler = async()=>{
-
-    try{
-      const res = await axios.post(`${USER_API_ENDPOINT}/logout`,{
-        withCredentials:true,
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.post(`${USER_API_ENDPOINT}/logout`, {
+        withCredentials: true,
       });
 
-      if (res.data.success){
+      if (res.data.success) {
         dispatch(setUser(null));
         navigate("/");
         toast.success(res.data.message);
-        
-      }else{
+      } else {
         console.error("Error logging out:", res.data);
       }
-    }catch(error){
+    } catch (error) {
       console.error("Axios error:,", error);
-      if (error.response){
-        console.error("Error response: ", error.response.data)
+      if (error.response) {
+        console.error("Error response: ", error.response.data);
       }
-      toast.error("Error logging out. Please try again.")
+      toast.error("Error logging out. Please try again.");
     }
-  }
+  };
 
   return (
     <div className="bg-white shadow-sm border-b border-gray-200">
@@ -55,38 +51,53 @@ const Navbar = () => {
         {/* Navigation Links */}
         <div className="flex items-center gap-8">
           <ul className="flex font-medium gap-6 items-center text-[#2C3E50]">
-            <li>
-              <Link
-                to="/"
-                className="hover:text-[#E67E22] transition-all duration-300 text-base"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/browse"
-                className="hover:text-[#E67E22] transition-all duration-300 text-base"
-              >
-                Browse
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/jobs"
-                className="hover:text-[#E67E22] transition-all duration-300 text-base"
-              >
-                Jobs
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/companies"
-                className="hover:text-[#E67E22] transition-all duration-300 text-base"
-              >
-                Companies
-              </Link>
-            </li>
+            {user && user.role === "Recruiter" ? (
+              <>
+                <li>
+                  <Link
+                    to="/admin/companies"
+                    className="hover:text-[#E67E22] transition-all duration-300 text-base"
+                  >
+                    Jobs
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="admin/jobs"
+                    className="hover:text-[#E67E22] transition-all duration-300 text-base"
+                  >
+                    Companies
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link
+                    to="/"
+                    className="hover:text-[#E67E22] transition-all duration-300 text-base"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/browse"
+                    className="hover:text-[#E67E22] transition-all duration-300 text-base"
+                  >
+                    Browse
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/jobs"
+                    className="hover:text-[#E67E22] transition-all duration-300 text-base"
+                  >
+                    Jobs
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
 
           {/* User Login/Signup or Avatar */}
@@ -121,21 +132,23 @@ const Navbar = () => {
                     <AvatarFallback>CN</AvatarFallback>
                   </Avatar>
                   <div>
-                    <h1 className="font-medium text-[#2C3E50]">{user?.fullname}</h1>
+                    <h1 className="font-medium text-[#2C3E50]">
+                      {user?.fullname}
+                    </h1>
                     <p className="text-sm text-gray-500">
                       {user?.profile?.bio}
                     </p>
                   </div>
                 </div>
                 <div className="flex flex-col space-y-2">
-                <Link to="/profile">
-                  <Button
-                    variant="ghost"
-                    className="w-full justify-start gap-2 hover:bg-gray-100 text-[#2C3E50]"
-                  >
-                    <User2 size={16} />
-                    Profile
-                  </Button>
+                  <Link to="/profile">
+                    <Button
+                      variant="ghost"
+                      className="w-full justify-start gap-2 hover:bg-gray-100 text-[#2C3E50]"
+                    >
+                      <User2 size={16} />
+                      Profile
+                    </Button>
                   </Link>
                   <Button
                     onClick={logoutHandler}
