966f265216bae7c9e11fc8a78922daf46033afe6
aryastha77 <aryashrestha888@gmail.com>
2025-03-27 13:35:19 +0545

    Admin route added

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -7,6 +7,8 @@ import Jobs from './components/components_lite/Jobs.jsx'
 import Browse from './components/components_lite/Browse.jsx'
 import Description from './components/components_lite/Description.jsx'
 import Profile from './components/components_lite/Profile.jsx'
+import Companies from './components/admincomponents/Companies.jsx'
+import CompanyCreate from './components/admincomponents/CompanyCreate.jsx'
 
 const appRouter = createBrowserRouter([
   {
@@ -29,7 +31,15 @@ const appRouter = createBrowserRouter([
   },
   {
     path: '/description/:id', element: <Description />
-  }
+  },
+  //admin
+  {
+    path:"/admin/companies",element: <Companies />
+  },
+  {
+    path:"/admin/companies/create",element: <CompanyCreate />
+  },
+ 
 ]);
 
 function App() {
