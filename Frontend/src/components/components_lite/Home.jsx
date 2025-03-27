509a348806f48ff5310799b1a64765de6dc0c1d4
aryastha77 <aryashrestha888@gmail.com>
2025-03-27 13:34:07 +0545

    Admin route added

--- Frontend/src/components/components_lite/Home.jsx
+++ Frontend/src/components/components_lite/Home.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar';
 import Header from './Header';
 import Categories from './Categories';
 import LatestJobs from './LatestJobs';
 import Footer from './Footer';
 import useGetAllJobs from '@/hooks/useGetAllJobs';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = () => {
   useGetAllJobs();
+
+  
+const {user} = useSelector((store)=>store.auth);
+const navigate = useNavigate();
+
+useEffect(()=>{
+  if (user?.role === 'Recruiter'){
+    navigate("/admin/companies");
+  }
+},[])
   
   return (
     <div>
