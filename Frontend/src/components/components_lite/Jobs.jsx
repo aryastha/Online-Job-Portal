cad0127076c0f16ec2bbaf9a8f60f602c045169b
aryastha77 <aryashrestha888@gmail.com>
2025-04-13 11:53:24 +0545

    Filter Feature Updated

--- Frontend/src/components/components_lite/Jobs.jsx
+++ Frontend/src/components/components_lite/Jobs.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar.jsx";
 import FilterCard from "./FilterCard.jsx";
 import Job1 from "./Job1.jsx";
 import { useSelector } from "react-redux";
+import {motion} from 'framer-motion';
+import { useEffect } from "react";
 
 const Jobs = () => {
 
-  const { allJobs } = useSelector((store) => store.job);  
+  const { allJobs , searchedQuery} = useSelector((store) => store.job); 
+  const [filterJobs, setFilterJobs] = useState (allJobs);
+
+
+  useEffect(()=>{
+    if (!searchedQuery || searchedQuery === ""){
+      setFilterJobs(allJobs);
+      return;
+    }
+
+    const filteredJobs = allJobs.filter((job) =>{
+      const query = searchedQuery.toLowerCase();
+      return(
+        job.title?.toLowerCase().includes(query) ||
+        job.description?.toLowerCase().includes(query) ||
+        job.location?.toLowerCase().includes(query) ||
+        job.experience?.toLowerCase().includes(query) ||
+        job.salary?.toLowerCase().includes(query)
+      );
+    });
+
+    setFilterJobs(filteredJobs);
+
+  },[allJobs, searchedQuery])
   return (
     <div>
       <Navbar />
@@ -18,15 +43,21 @@ const Jobs = () => {
           </div>
 
           {/* Job List - Takes 80% of the width */}
-          {allJobs.length <= 0 ? (
+          {filterJobs.length <= 0 ? (
             <span className="text-gray-600">No jobs found</span>
           ) : (
             <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"> {/* Responsive grid */}
-                {allJobs.map((job) => (
-                  <div key={job._id}>
+                {filterJobs.map((job) => (
+                  <motion.div 
+                  key={job.id}
+                  initial={{opacity: 0, x:100}}
+                  animate={{opacity: 1, x: 0}}
+                  exit= {{opacity: 0, x:-100}}
+                  transition= {{duration: 0.4}}
+                  >
                     <Job1  job={job}/>
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </div>
